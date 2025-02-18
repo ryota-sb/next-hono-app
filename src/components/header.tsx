9d40bad6874777cf44f95e402351c1721f11e79c
ryota-sb <ryota.sb.1206@gmail.com>
2025-02-19 00:38:19 +0900

    feat: add sign out button by session condition

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,13 @@
-import { signIn } from "@/auth"
+import { auth, signIn, signOut} from "@/auth"
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth()
+
   return (
     <header className="h-[70px] border-b">
       <div className="container mx-auto h-full flex items-center justify-between">
         <h1 className="text-[1.5rem] font-bold">Next Hono App</h1>
-        <form
+        {!session && <form
           action={async () => {
             "use server"
             await signIn("github")
@@ -17,7 +19,21 @@ export default function Header() {
           >
             Sign in with GitHub
           </button>
-        </form>
+        </form>}
+
+        {session && <form
+          action={async () => {
+            "use server"
+            await signOut()
+          }}
+        >
+          <button
+            type="submit"
+            className="bg-yellow-300 py-1 px-3 rounded-full font-bold hover:bg-yellow-400 hover:shadow-md transition-all duration-200 ease-in-out active:scale-95"
+          >
+            Sign out
+          </button>
+        </form>}
       </div>
     </header>
   )
