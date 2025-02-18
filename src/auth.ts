6c44bc6bdb634781b3772c3260edcff0a7e1dba8
ryota-sb <ryota.sb.1206@gmail.com>
2025-02-18 23:49:46 +0900

    feat: linking prisma and auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
+import { prisma } from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 })
