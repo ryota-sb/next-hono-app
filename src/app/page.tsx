21212b6a601c116cde99d6bc38d696593913918b
ryota-sb <ryota.sb.1206@gmail.com>
2025-02-20 20:30:38 +0900

    fix: modify layout of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+import { hono } from '@/lib/hono'
+
+export default async function Home() {
+  const res = await hono.api.blogs.$get()
+  const blogs = await res.json()
+
+  if (blogs.length === 0) { return <div>まだ投稿がありません</div> }
+
   return (
-   <></>
+   <div>
+    {blogs.map((blog) => (
+      <div key={blog.id}>
+        <h2>{blog.title}</h2>
+        <p>{blog.content}</p>
+      </div>
+    ))}
+   </div>
   );
 }
